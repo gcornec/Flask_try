1f75d2c3e719b382c297669bee3c8f7a89a82a6d
Gilles <cornec.gilles@gmail.com>
2019-09-20 12:25:06 +0200

    add worker for redis queueing

---
+++ static/main.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+
+  angular.module('WordcountApp', [])
+
+  .controller('WordcountController', ['$scope', '$log', '$http',
+    function($scope, $log, $http) {
+
+    $scope.getResults = function() {
+
+      $log.log("test");
+
+      // get the URL from the input
+      var userInput = $scope.url;
+
+      // fire the API request
+      $http.post('/start', {"url": userInput}).
+        success(function(results) {
+          $log.log(results);
+        }).
+        error(function(error) {
+          $log.log(error);
+        });
+
+    };
+
+  }
+]);
+)
